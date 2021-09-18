6fa417ee9d4679615c7ef3147a61dbf5e7c31e95
chamoddissanayake <dissanayakechamod@gmail.com>
2021-09-18 20:07:06 +0530

    minor modifications

--- Picmash - Web/src/components/home-items.jsx
+++ Picmash - Web/src/components/home-items.jsx
@@ -11,6 +11,13 @@ export default class HomeItems extends Component {
 
     }
 
+    SaveItemInDrive() {
+        console.log("Save item in drive")
+
+
+
+    }
+
     render() {
         return (
             <div>
@@ -21,29 +28,28 @@ export default class HomeItems extends Component {
                         <div className="row">
 
                             <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                                <a href="">
-                                    <div className="card-flyer">
-                                        <div className="text-box">
-                                            <div className="image-box">
-                                                <img
-                                                    src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg"
-                                                    alt=""/>
-                                            </div>
-                                            <div className="text-container">
-                                                <h6>Title 01AA</h6>
-                                                <p className="justify-card">Lorem Ipsum is simply dummy text of the
-                                                    printing and typesetting
-                                                    industry. Lorem Ipsum has been the industry's standard dummy text
-                                                    ever since the 1500s.</p>
-                                            </div>
-                                            <div className="card-bottom-div">
-                                                <p className="price-font" style={{"fontSize": "20px"}}>$ 10.00</p>
-                                                <button type="button" className="btn btn-primary">Try for Free Now
-                                                </button>
+                                <div className="card-flyer">
+                                    <div className="text-box">
+                                        <div className="image-box">
+                                            <img
+                                                src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg"
+                                                alt=""/>
+                                        </div>
+                                        <div className="text-container">
+                                            <h6>Title 01AA</h6>
+                                            <p className="justify-card">Lorem Ipsum is simply dummy text of the
+                                                printing and typesetting
+                                                industry. Lorem Ipsum has been the industry's standard dummy text
+                                                ever since the 1500s.</p>
+                                        </div>
+                                        <div className="card-bottom-div">
+                                            <p className="price-font" style={{"fontSize": "20px"}}>$ 10.00</p>
+                                            <div className="btn btn-primary"
+                                                 onClick={this.SaveItemInDrive}>Try for Free Now
                                             </div>
                                         </div>
                                     </div>
-                                </a>
+                                </div>
                             </div>
 
                             <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
