8f2c8e5542daa4dd0030b667d5449fc6496b7213
chamoddissanayake <dissanayakechamod@gmail.com>
2021-09-19 17:57:01 +0530

    remove general part

--- Picmash - Web/src/components/sell.jsx
+++ Picmash - Web/src/components/sell.jsx
@@ -9,11 +9,8 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import AuthService from "../services/auth.service";
 
-// import GooglePicker from '../';
 import GooglePicker from 'react-google-picker';
 
-// import  { useEffect } from 'react';
-// import useDrivePicker from 'react-google-drive-picker'
 
 export default class Sell extends Component {
 
@@ -21,7 +18,6 @@ export default class Sell extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            file_select_type: 'local',
 
             selected_file_type:'',  //image, video
 
@@ -30,20 +26,12 @@ export default class Sell extends Component {
             google_drive_video_preview :''
         };
 
-        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
     }
 
-    handleChange(event) {
-        if (event.target.value === 'local') {
-            this.setState({file_select_type: 'local'});
-        } else if (event.target.value === 'drive') {
-            this.setState({file_select_type: 'drive'});
-        }
 
-    }
 
     chooseGoogleFileBtnPressed() {
 
@@ -133,109 +121,79 @@ export default class Sell extends Component {
 
                             <div className="row">
                                 <div className="col-25">
-                                    <label>Choose Image</label>
+                                    <label>Choose Image/Video</label>
                                 </div>
 
 
                                 <div className="col-75">
-                                    <RadioGroup aria-label="resource-select-type" name="resource-select-type"
-                                                onChange={this.handleChange} value={this.state.file_select_type}>
-                                            <span>
-                                                <div className="form-check form-check-inline">
-                                                    <FormControlLabel value="local" control={<Radio/>}
-                                                                      label="Local File"/>
-                                                </div>
-                                                <div className="form-check form-check-inline">
-                                                    <FormControlLabel value="drive" control={<Radio/>} label="Google"/>
-                                                </div>
-                                            </span>
-                                    </RadioGroup>
-                                    {/*value={value} onChange={handleChange}*/}
-                                    <div className="col-75">
+                                    <div className="selected-choose-type-container">
+                                        {/*<input onClick={this.chooseGoogleFileBtnPressed} type="button" value="Choose image from Google Drive"/>*/}
+                                        {/*<button onClick={this.handleOpenPicker}>Open Picker</button>*/}
 
-                                        {this.state.file_select_type === "local" && <span>
-                                        <div className="selected-choose-type-container">
-                                            {/*Local Image Picker start*/}
-                                            {/*<p>Local file selector</p>*/}
-                                            <input type="file"
-                                                   id="avatar" name="avatar"
-                                                   accept="image/png, image/jpeg"/>
-                                            {/*Local Image picker end*/}
-                                        </div>
-                                    </span>}
+                                        <GooglePicker clientId={'801360513499-u7sc20pvp4nkkigtgvnnaajp4hcq4ate.apps.googleusercontent.com'}
+                                                      developerKey={'AIzaSyBJG2E08YMitCRBQSzyuJX6I57MOhfXrRs'}
+                                                      scope={['https://www.googleapis.com/auth/drive.readonly']}
+                                                      onChange={data => console.log('on change:', data)}
+                                                      onAuthFailed={data => console.log('on auth failed:', data)}
+                                                      multiselect={true}
+                                                      navHidden={true}
+                                                      authImmediate={false}
+                                            // mimeTypes={['image/png', 'image/jpeg', 'image/jpg']}
+                                                      query={'a query string like .txt or fileName'}
+                                                      viewId={'DOCS'}  onChange={data => console.log('on change:', data)}
+                                                      onAuthFailed={data => console.log('on auth failed:', data)}
+                                                      multiselect={true}
+                                                      navHidden={true}
+                                                      authImmediate={false}
+                                            // viewId={'FOLDERS'}
+                                                      createPicker={ (google, oauthToken) => {
+                                                          const googleViewId = google.picker.ViewId.FOLDERS;
+                                                          const docsView = new google.picker.DocsView(googleViewId)
+                                                              .setIncludeFolders(true)
+                                                              // .setMimeTypes('application/vnd.google-apps.folder')
+                                                              .setMimeTypes('application/vnd.google-apps.file')
+                                                              .setSelectFolderEnabled(true);
 
-                                        {this.state.file_select_type === "drive" && <span>
-                                        <div className="selected-choose-type-container">
-                                            {/*<input onClick={this.chooseGoogleFileBtnPressed} type="button" value="Choose image from Google Drive"/>*/}
-                                            {/*<button onClick={this.handleOpenPicker}>Open Picker</button>*/}
-
-                                            <GooglePicker clientId={'801360513499-u7sc20pvp4nkkigtgvnnaajp4hcq4ate.apps.googleusercontent.com'}
-                                                          developerKey={'AIzaSyBJG2E08YMitCRBQSzyuJX6I57MOhfXrRs'}
-                                                          scope={['https://www.googleapis.com/auth/drive.readonly']}
-                                                          onChange={data => console.log('on change:', data)}
-                                                          onAuthFailed={data => console.log('on auth failed:', data)}
-                                                          multiselect={true}
-                                                          navHidden={true}
-                                                          authImmediate={false}
-                                                          // mimeTypes={['image/png', 'image/jpeg', 'image/jpg']}
-                                                          query={'a query string like .txt or fileName'}
-                                                          viewId={'DOCS'}  onChange={data => console.log('on change:', data)}
-                                                          onAuthFailed={data => console.log('on auth failed:', data)}
-                                                          multiselect={true}
-                                                          navHidden={true}
-                                                          authImmediate={false}
-                                                          // viewId={'FOLDERS'}
-                                                          createPicker={ (google, oauthToken) => {
-                                                              const googleViewId = google.picker.ViewId.FOLDERS;
-                                                              const docsView = new google.picker.DocsView(googleViewId)
-                                                                  .setIncludeFolders(true)
-                                                                  // .setMimeTypes('application/vnd.google-apps.folder')
-                                                                  .setMimeTypes('application/vnd.google-apps.file')
-                                                                  .setSelectFolderEnabled(true);
-
-                                                              const picker = new window.google.picker.PickerBuilder()
-                                                                  .addView(docsView)
-                                                                  .setOAuthToken(oauthToken)
-                                                                  // .setDeveloperKey('AIzaSyBJG2E08YMitCRBQSzyuJX6I57MOhfXrRs')
-                                                                  .setCallback((data)=>{
+                                                          const picker = new window.google.picker.PickerBuilder()
+                                                              .addView(docsView)
+                                                              .setOAuthToken(oauthToken)
+                                                              // .setDeveloperKey('AIzaSyBJG2E08YMitCRBQSzyuJX6I57MOhfXrRs')
+                                                              .setCallback((data)=>{
 
 
 
-                                                                      if(data.action === 'picked'){
+                                                                  if(data.action === 'picked'){
 
-                                                                          console.log(data.docs[0].mimeType)
-                                                                          if(data.docs[0].mimeType=== 'image/jpeg'){
-                                                                              this.setState({
-                                                                                  selected_file_type:'image',
-                                                                                  google_selected_file_data: data.docs[0]
-                                                                              }, () => {
-                                                                                  this.fillGoogleDriveImagePreviewer();
-                                                                              });
-                                                                          }else if(data.docs[0].mimeType=== 'video/mp4'){
-                                                                              this.setState({
-                                                                                  selected_file_type:'video',
-                                                                                  google_selected_file_data: data.docs[0]
-                                                                              }, () => {
-                                                                                  this.fillGoogleDriveVideoPreviewer();
-                                                                              });
-                                                                          }else{
-                                                                              alert('Invalid file selected. Please select a photo or video')
-                                                                          }
-
-
+                                                                      console.log(data.docs[0].mimeType)
+                                                                      if(data.docs[0].mimeType=== 'image/jpeg'){
+                                                                          this.setState({
+                                                                              selected_file_type:'image',
+                                                                              google_selected_file_data: data.docs[0]
+                                                                          }, () => {
+                                                                              this.fillGoogleDriveImagePreviewer();
+                                                                          });
+                                                                      }else if(data.docs[0].mimeType=== 'video/mp4'){
+                                                                          this.setState({
+                                                                              selected_file_type:'video',
+                                                                              google_selected_file_data: data.docs[0]
+                                                                          }, () => {
+                                                                              this.fillGoogleDriveVideoPreviewer();
+                                                                          });
+                                                                      }else{
+                                                                          alert('Invalid file selected. Please select a photo or video')
                                                                       }
 
-                                                                  });
 
-                                                              picker.build().setVisible(true);
-                                                          }}
-                                            >
-                                                <span>Click</span>
-                                                <div className="google"></div>
-                                            </GooglePicker>
+                                                                  }
 
-                                        </div>
-                                    </span>}
+                                                              });
+
+                                                          picker.build().setVisible(true);
+                                                      }}
+                                        >
+                                            <span>Click here to choose file from google drive</span>
+                                            <div className="google"></div>
+                                        </GooglePicker>
 
                                     </div>
                                 </div>
